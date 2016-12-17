c2d4b6cac2050938116b6aa4488478223eafa90d
icarter09 <icarter1391@gmail.com>
2016-12-17 17:03:31 -0500

    Update CryptoExample.js
    Added assertions.

--- CryptoExample.js
+++ CryptoExample.js
@@ -1,12 +1,14 @@
-var crypto = require('crypto');
+const crypto = require('crypto');
+const assert = require('assert');
 
 const cipher = crypto.createCipher('aes192', 'a password');
 const decipher = crypto.createDecipher('aes192', 'a password');
 
 var encrypted = cipher.update('hello world', 'utf8', 'hex');
 encrypted += cipher.final('hex');
-console.log(encrypted);
 
 var decrypted = decipher.update(encrypted, 'hex', 'utf8');
 decrypted += decipher.final('utf8');
-console.log(decrypted);
+
+assert.equal(decrypted, 'hello world');
+assert.notEqual(decrypted, 'derp derp');
