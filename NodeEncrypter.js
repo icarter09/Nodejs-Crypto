07ca42ec365ac78939588ec404137d02d095c0d3
icarter09 <icarter1391@gmail.com>
2016-12-18 19:33:42 -0500

    Update NodeEncrypter.js
    Cleaned up code.

--- NodeEncrypter.js
+++ NodeEncrypter.js
@@ -1,33 +1,93 @@
 const crypto   = require('crypto');
 const readlineSync = require('readline-sync');
 
-console.log("Welcome to NodeEncrypter!")
-var options = ['encrypt', 'decrypt'];
-var choice = readlineSync.keyInSelect(options, 'What would you like to do?');
-
-var key, index, textToEncrypt, textToDecrypt, encrypted, decrypted;
+var key, index;
+var textToEncrypt, textToDecrypt;
+var encrypted, decrypted;
+var options, algo;
 var cipher, decipher;
-var algo = [ "aes128", "aes192", "aes256", "des3", "desx", "rc2"];
 
+initializeArrays();
+displayWelcomeText();
+
+var choice = startProgram();
 if (options[choice] == 'encrypt') {
-    key = readlineSync.question('Enter a key for the encryption cipher: ');
-    index = readlineSync.keyInSelect(algo, 'Select an encryption algorithm');
-    cipher = crypto.createCipher(algo[index], key);
-    textToEncrypt = readlineSync.question('Enter text to encrypt: ', {
-        hideEchoBack: true    
-    });
+    key = encryptionKeyQuestion();
+    index = encryptionAlgorithmQuestion(); 
+    if (index == -1) {
+        return true;
+    }
+    cipher = createEncryptionCipher(index, key);
+    textToEncrypt = encryptTextQuestion();
     encrypted = cipher.update(textToEncrypt, 'utf8', 'hex');     
     encrypted += cipher.final('hex');
-    console.log("Encrypted text = " + encrypted);
-    
+    displayEncryptedText();
 } else if (options[choice] == 'decrypt') {
-    key = readlineSync.question('Enter a key for the decryption cipher: ');
-    index = readlineSync.keyInSelect(algo, 'Select an algorithm for decryption');
-    decipher = crypto.createDecipher(algo[index], key);
-    textToDecrypt = readlineSync.question("Enter text to decrypt: ");
+    key = decryptionKeyQuestion();
+    index = decryptionAlgorithmQuestion();
+    if (index == -1) {
+        return true;
+    }
+    decipher = createDecryptionCipher(index, key);
+    textToDecrypt = decryptTextQuestion();
     decrypted = decipher.update(textToDecrypt, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
-    console.log("Decrypted text = " + decrypted);
+    displayDecryptedText();
 } else {
     return true;    
+}
+
+function initializeArrays() {
+    options = ['encrypt', 'decrypt'];
+    algo = [ "aes128", "aes192", "aes256", "des3", "desx", "rc2"];
+}
+
+function displayWelcomeText() {
+    console.log("Welcome to NodeEncrypter!")
+}
+
+function startProgram() {
+    return readlineSync.keyInSelect(options, 'What would you like to do?');
+}
+
+function displayDecryptedText() {
+    console.log("Decrypted text = " + decrypted);
+}
+
+function decryptTextQuestion() {
+    return readlineSync.question("Enter text to decrypt: ");
+}
+
+function createDecryptionCipher(index, key) {
+    return crypto.createDecipher(algo[index], key);
+}
+
+function decryptionAlgorithmQuestion() {
+    return readlineSync.keyInSelect(algo, 'Select an algorithm for decryption');
+}
+
+function decryptionKeyQuestion() {
+    return readlineSync.question('Enter a key for the decryption cipher: ');
+}
+
+function encryptionKeyQuestion() {
+    return readlineSync.question('Enter a key for the encryption cipher: ');
+}
+
+function encryptionAlgorithmQuestion() {
+    return readlineSync.keyInSelect(algo, 'Select an encryption algorithm');
+}
+
+function createEncryptionCipher(index, key) {
+    return crypto.createCipher(algo[index], key);
+}
+
+function encryptTextQuestion() { 
+    return readlineSync.question('Enter text to encrypt: ', {
+        hideEchoBack: true    
+    });
+}
+
+function displayEncryptedText() {
+    console.log("Encrypted text = " + encrypted);
 }
